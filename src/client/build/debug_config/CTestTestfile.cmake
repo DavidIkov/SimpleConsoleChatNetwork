79260c51671b7034b598d8939b7394fff6eb6343
Davidikov <94567291+DavidIkov@users.noreply.github.com>
2025-01-14 17:34:47 +0300

    Delete src/client/build/debug_config directory

--- src/client/build/debug_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/David/Desktop/networkProgrammingLearning/src/client
-# Build directory: C:/Users/David/Desktop/networkProgrammingLearning/src/client/build/debug_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
